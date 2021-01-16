a34fa766423c992767bd06ac267d89c679f2e583
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-16 11:59:52 -0700

    fix js event listener

--- web/AssignmentOne/script.js
+++ web/AssignmentOne/script.js
@@ -1,10 +1,10 @@
-function testFunction() {
+document.getElementById("testButton").addEventListener("click", function() {
 	var xmlhttp=new XMLHttpRequest();
-	  xmlhttp.onreadystatechange=function() {
-	    if (this.readyState==4 && this.status==200) {
+	xmlhttp.onreadystatechange=function() {
+		if (this.readyState==4 && this.status==200) {
 			document.getElementById("testDiv").innerHtml = this.responseText;
-	    }
-	  }
-	  xmlhttp.open("GET","./test.php?test=true");
-	  xmlhttp.send();
-}
+		}
+	}
+	xmlhttp.open("GET","./test.php?test=true");
+	xmlhttp.send();
+})
