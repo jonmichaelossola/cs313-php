4785213a5fb2a5affdcfbf76e1963a897c6bacc9
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-16 12:07:10 -0700

    Darn misspelled Javascript

--- web/AssignmentOne/script.js
+++ web/AssignmentOne/script.js
@@ -2,7 +2,7 @@ document.getElementById("testButton").addEventListener("click", function() {
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
 		if (this.readyState==4 && this.status==200) {
-			document.getElementById("testDiv").innerHtml = this.responseText;
+			document.getElementById("testDiv").innerHTML = this.responseText;
 		}
 	}
 	xmlhttp.open("GET","./test.php?test=true");
