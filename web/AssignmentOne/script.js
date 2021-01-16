7266ea9446a5ac62a86a09571fbc4c0b0166d882
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-16 12:08:23 -0700

    last fix I swear

--- web/AssignmentOne/script.js
+++ web/AssignmentOne/script.js
@@ -2,7 +2,7 @@ document.getElementById("testButton").addEventListener("click", function() {
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
 		if (this.readyState==4 && this.status==200) {
-			document.getElementById("testDiv").innerHTML = this.responseText;
+			document.getElementById("test").innerHTML = this.responseText;
 		}
 	}
 	xmlhttp.open("GET","./test.php?test=true");
