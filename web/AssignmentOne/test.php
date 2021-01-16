a9ebe24b43bffe8a232e88eac3648a87fba8838e
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-16 11:45:35 -0700

    add first assignment to test some stuff

---
+++ web/AssignmentOne/test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$q=$_GET["q"];
+	header('Access-Control-Allow-Origin: *');
+
+	header('Access-Control-Allow-Methods: GET, POST');
+
+	header("Access-Control-Allow-Headers: X-Requested-With");
+
+	echo("test");
+?>
+
+</body>
+</html>
