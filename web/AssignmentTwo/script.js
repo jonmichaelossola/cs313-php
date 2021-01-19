2e75399340262d6cf611419f934cf3809c800f54
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-18 17:15:55 -0700

    add stuff for testing

--- web/AssignmentTwo/script.js
+++ web/AssignmentTwo/script.js
@@ -5,7 +5,8 @@ function testFunction() {
 	let xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
 		if (this.readyState === 4 && this.status === 200) {
-			document.getElementById().innerHTML = this.responseText;
+			console.log(this.responseText);
+			document.getElementById("test").innerHTML = this.responseText;
 		}
 	}
 	xmlhttp.open("GET", "./eventsHandler.php?getItems=true");
