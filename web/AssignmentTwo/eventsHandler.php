2d61ee90dfd2909aaebca7f85f59c030c32b58d9
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-19 19:31:54 -0700

    adding stuff

--- web/AssignmentTwo/eventsHandler.php
+++ web/AssignmentTwo/eventsHandler.php
@@ -5,10 +5,35 @@
 		$_SESSION["numItems"] = 0;
 	}
 
+	if (isset($_GET["getNumItems"])) {
+		echo $_SESSION["numItems"];
+	}
+
+	if (isset($_GET["getItems"])) {
+		$temp = array();
+		array_push($temp, $_SESSION["cart"]);
+		array_push($temp, $_SESSION["numItems"]);
+		$myJson = json_encode($temp);
+		echo $myJson;
+	}
+
 	if (isset($_GET['item'])) {
 		$item = $_GET['item'];
 		array_push($_SESSION["cart"], $item);
 		$_SESSION["numItems"] += 1;
+		$temp = array();
+		array_push($temp, $_SESSION["cart"]);
+		array_push($temp, $_SESSION["numItems"]);
+		$myJson = json_encode($temp);
+		echo $myJson;
+	}
+
+	if (isset($_GET['removeItem'])) {
+		$item = $_GET['removeItem'];
+		if (($key = array_search($item, $_SESSION["cart"])) !== false) {
+			unset($_SESSION["cart"][$key]);
+			$_SESSION["numItems"] -= 1;
+		}
 		$temp = array();
 		array_push($temp, $_SESSION["cart"]);
 		array_push($temp, $_SESSION["numItems"]);
--- web/AssignmentTwo/script.js
+++ web/AssignmentTwo/script.js
@@ -1,13 +1,28 @@
 console.log('hello?');
 
-function testFunction() {
+function getItems(reason) {
 	// Make request to server for shopping cart items
 	// Should return html list of items to put into <ul>
 	let xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
 		if (this.readyState === 4 && this.status === 200) {
-			console.log(this.responseText);
-			// document.getElementById("test").innerHTML = this.responseText;
+			const data = JSON_parse(this.responseText);
+			console.log(data);
+			if (reason === "cart") {
+				// parse items into items unordered list
+				let str = "";
+				if (data[0].length > 0) {
+					data[0].map(item => {
+						str += `<li><h3>${item}</h3><div><button data-animal=${item} onclick="removeFromCart(event)">Remove From Cart</button></div></li>`
+					})
+				} else {
+					str = "<li><h2>You have no items in your cart</h2></li>"
+				}
+				document.getElementById("cartItemsHeader").innerHTML = `Your Cart Items (${data[1]})`;
+				document.getElementById("cartItems").innherHTML = str;
+			} else if (reason === "index") {
+				// do stuff
+			}
 		}
 	}
 	xmlhttp.open("GET", "./eventsHandler.php?getItems=true");
@@ -15,7 +30,6 @@ function testFunction() {
 }
 
 function addToCart(e) {
-	console.log(e.target);
 	let xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
 		if (this.readyState === 4 && this.status === 200) {
@@ -25,5 +39,16 @@ function addToCart(e) {
 		}
 	}
 	xmlhttp.open("GET", `./eventsHandler.php?item=${e.target.getAttribute("data-animal")}`);
+	xmlhttp.send();
+}
+
+function removeFromCart(e) {
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function() {
+		if (this.readyState === 4 && this.status === 200) {
+			var test = JSON.parse(this.responseText);
+		}
+	}
+	xmlhttp.open("GET", `./eventsHandler.php?removeItem=${e.target.getAttribute("data-animal")}`);
 	xmlhttp.send();
 }
