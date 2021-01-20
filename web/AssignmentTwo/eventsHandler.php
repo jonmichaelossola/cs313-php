4881dcf6d5fc810f0a95fc072d1908edddaec461
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-01-19 18:50:46 -0700

    adding stuff

--- web/AssignmentTwo/eventsHandler.php
+++ web/AssignmentTwo/eventsHandler.php
@@ -1,5 +1,19 @@
 <?php
-	if (isset($_GET['getItems'])) {
-		echo "<li>test</li><li>test two</li><li>test three</li>";
+	session_start();
+	$_SESSION["cart"]
+	if (!isset($_SESSION["cart"])) {
+		$_SESSION["cart"] = array();
 	}
+
+	if (isset($_GET['getItems'])) {
+		echo "<li>Bear<div><button>Add To Cart</button></div></li><li>Cougar<div><button>Add To Cart</button></div></li><li>Panda<div><button>Add To Cart</button></div></li><li>Lion<div><button>Add To Cart</button></div></li><li>Giraffe</li>";
+	}
+
+	if (isset($_GET['item'])) {
+		$item = $_GET['item'];
+		array_push($_SESSION["cart"], $item);
+		$myJson = json_encode($_SESSION["cart"]);
+		echo $myJson;
+	}
+
 ?>
--- web/AssignmentTwo/script.js
+++ web/AssignmentTwo/script.js
@@ -7,9 +7,24 @@ function testFunction() {
 	xmlhttp.onreadystatechange=function() {
 		if (this.readyState === 4 && this.status === 200) {
 			console.log(this.responseText);
-			document.getElementById("test").innerHTML = this.responseText;
+			// document.getElementById("test").innerHTML = this.responseText;
 		}
 	}
 	xmlhttp.open("GET", "./eventsHandler.php?getItems=true");
+	xmlhttp.send();
+}
+
+function addToCart(e) {
+	console.log(e.target);
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function() {
+		if (this.readyState === 4 && this.status ===200) {
+			console.log(this.responseText);
+			var test = JSON.parse(responseText);
+			// do stuff
+			console.log(test);
+		}
+	}
+	xmlhttp.open("GET", `./eventsHandler.php?item=${e.target.getAttribute("data-animal")}`);
 	xmlhttp.send();
 }
