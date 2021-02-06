4492b22250c726c965ad78f2cc75407643522c95
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-02-05 18:09:19 -0700

    adding stuff

--- web/week5/eventsHandler.php
+++ web/week5/eventsHandler.php
@@ -1,32 +1,24 @@
 <?php
-  try
-  {
-    $dbUrl = getenv('DATABASE_URL');
+  $dbUrl = getenv('DATABASE_URL');
 
-    $dbOpts = parse_url($dbUrl);
+  $dbOpts = parse_url($dbUrl);
 
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   if (isset($_POST["username"]) && isset($_POST["password"])) {
-    echo $_POST["username"];
-    foreach ($db->query('SELECT name, password FROM players WHERE name=' . $_POST['username'] . ' AND password=' . $_POST['password']) as $row)
-    {
-      echo $row["name"] . $row["password"] . $row["id"];
-    }
+    echo $_POST["password"];
+    // foreach ($db->query('SELECT * FROM players WHERE name=' . $_POST['username'] . ' AND password=' . $_POST['password']) as $row)
+    // {
+    //   echo $row["name"] . $row["password"] . $row["id"];
+    // }
   }
 
   // if (isset($_GET["id"]) && isset($_GET["posts"])) {
