dac70dfe671080648877c3be0e95fb6e38d590fe
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-02-12 18:48:30 -0700

    adding stuff

--- web/week5/eventsHandler.php
+++ web/week5/eventsHandler.php
@@ -75,11 +75,11 @@
     }
     $id+=1;
     // Insert player into database
-    $stmt = $db->prepare('INSERT INTO players VALUES (:username, :city, :id, :passphrase)');
+    $stmt = $db->prepare('INSERT INTO players (name, city, id, password) VALUES (:username, :city, :id, :passphrase)');
     $stmt->bindValue(":username", $username, PDO::PARAM_STR);
-    $stmt->bindValue(":passphrase", $passphrase, PDO::PARAM_STR);
     $stmt->bindValue(":city", $city, PDO::PARAM_STR);
     $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+    $stmt->bindValue(":passphrase", $passphrase, PDO::PARAM_STR);
     $stmt->execute();
   }
 
@@ -90,7 +90,7 @@
     $description = $_POST["description"];
     $timeInHours = $_POST["timeInHours"];
 
-    $stmt = $db->prepare('INSERT INTO posts (:playerID, :time, :location, :description, :timeInHours)');
+    $stmt = $db->prepare('INSERT INTO posts VALUES (:playerID, :time, :location, :description, :timeInHours)');
     $stmt->bindValue(":playerID", $playerID, PDO::PARAM_STR);
     $stmt->bindValue(":time", $time, PDO::PARAM_STR);
     $stmt->bindValue(":location", $location, PDO::PARAM_STR);
