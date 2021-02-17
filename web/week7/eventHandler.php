96ed1c847ae691097716d41da6c5de468f2d6339
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-02-16 17:47:40 -0700

    adding stuff

---
+++ web/week7/eventHandler.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  if (isset($_GET["test"])) {
+    echo 'SELECT * FROM posts WHERE playerid!=\'' . $_SESSION["userID"] . '\'';
+  }
+
+  if (isset($_POST["login"]) && isset($_POST["username"]) && isset($_POST["password"])) {
+    $name = "";
+    foreach ($db->query('SELECT * FROM players WHERE name=\'' . $_POST["username"] . '\' AND password=\'' . $_POST["password"] . '\'') as $row)
+    {
+      $name = $row["name"] . ' ' . $row["password"];
+    }
+    if (empty($name)) {
+      echo "failed";
+    }
+    $_SESSION["userID"] = $row["id"]; 
+    echo $name;
+  }
+
+  if (isset($_POST["register"])) {
+    // create username / passwd in db
+    $id = rand();
+    $username = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $passwd = $_POST["password"];
+
+    $stmt = $db->prepare('INSERT INTO users (id, username, password) VALUES (:id, :username, :password)');
+    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+    $stmt->bindValue(":username", $username, PDO::PARAM_STR);
+    $stmt->bindValue(":password", $passwd, PDO::PARAM_STR);
+    $stmt->bindValue(":passphrase", $passphrase, PDO::PARAM_STR);
+    $stmt->execute();
+    echo "registered";
+  }
+
+?>
---
+++ web/week7/script.js
@@ -0,0 +1,20 @@
+function submitRegistrationInfo() {
+	console.log('hello world');
+	// AJAX
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	let request = new XMLHttpRequest();
+	  request.open("POST", "./eventsHandler.php", true);
+	  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+	  request.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	      console.log(this.responseText);
+	      if (this.responseText !== "failed") {
+	        window.location.href = "./home.php";
+	      }
+	    }
+	  };
+
+	  request.send(`register=true&username=${username}&password=${password}`);
+}
